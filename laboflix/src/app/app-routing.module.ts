cba9ee44588955d3109e83ff729ac323d471b78a
Tatiana Cifuentes G <taciga30@gmail.com>
2024-01-19 02:30:01 -0500

    Project creation

--- laboflix/src/app/app-routing.module.ts
+++ laboflix/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { MovieDetailComponent } from './views/movie-detail/movie-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component:HomeComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'movie-detail', component:MovieDetailComponent},
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- laboflix/src/app/app.module.ts
+++ laboflix/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardMovieComponent } from './components/card-movie/card-movie.component';
+import { HomeComponent } from './views/home/home.component';
+import { MovieDetailComponent } from './views/movie-detail/movie-detail.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
+import { FiltersComponent } from './components/filters/filters.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardMovieComponent,
+    HomeComponent,
+    MovieDetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    PaginationComponent,
+    FiltersComponent,
   ],
   imports: [
     BrowserModule,
